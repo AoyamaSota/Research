7d26eb43e52766bcd8c4fbb1a5f9765a403a1956
Sota Aoyama <sotasota0921@gmail.com>
2016-09-03 20:32:15 +0900

    大きいドットのプログラム

--- ConsoleApplication1/Kalman_test.cpp
+++ ConsoleApplication1/Kalman_test.cpp
@@ -10,63 +10,9 @@
 #include"Header.h"
 
 #pragma warning(disable:4996)
-#define WIDTH 1280
-#define HEIGHT 960
 
 #define WIDTH 640
 #define HEIGHT 480
-
-//cv::Mat bin_img;
-//CvMat *h;
-//PointGrayCamera camera;
-//CvMoments moment;
-//cv::Point2d img_center;
-////CvPoint2D32f center[2] = {0,0};
-//CvPoint2D32f hand_center;
-//int i = 0, sum;
-//int flag;
-//cv::Mat mat2;
-//cv::Mat gray_img;
-//cv::Mat bininv_img, trunc_img, tozero_img, tozeroinv_img;
-//int counter = 0;
-//cv::Mat dst_img;
-//cv::Mat src_img;
-//cv::Mat projection;
-////cv::Mat
-//cv::Mat background;
-//cv::Mat pattern;
-//cv::Mat_<double> homo;
-////int count=0;
-//int j = 0;
-//int sign = 1;
-//double h_tmp[16] = { 0 };
-//cv::Size patch2(1280, 960);
-//cv::Size patch(640, 480);
-//DWORD startTime;
-//double timer;
-//int fps = 0;
-//int move = 0;
-//int condition;
-//// アニメーションの周期
-//#define CYCLE 1
-//// テクスチャオブジェクト
-//static GLuint texname;
-//
-//
-//
-//
-//// asa
-//cv::Mat tmpmat;
-//cv::Mat tmpmat2;
-//std::vector<cv::Point2d> tnkpoint;
-
-// カルマンフィルタ
-//CvKalman *kalman = cvCreateKalman(4, 2);
-//std::vector<cv::Point2d> center;
-//cv::Point2d posi;
-//cv::Point2d hand;
-//
-//std::vector<double> v;
 
 
 int main(int argc, char* argv[])
